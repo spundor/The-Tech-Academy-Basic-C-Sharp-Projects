52cd5cfdb84b70a264117cf180b8e7fbd9543002
scarb <scarboroughpundor@gmail.com>
2019-10-27 10:28:10 -0500

    Initial Commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main();
+
+        {
+        Console.WriteLine(“What is your name?”);
+        Console.WriteLine("Hello World!");
+        }
+    }
+}
