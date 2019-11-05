a78ac1774711d18befdde72f9cf888717b77ca79
scarb <scarboroughpundor@gmail.com>
2019-11-05 16:25:59 -0600

    new

---
+++ Step66_MathOps/Step66/Step66/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+
+namespace Step66_MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //User enter a number to be multiplied by 50, and print 
+            Console.WriteLine("Enter a number to be multiplied by 50:");
+            string inputMulti = Console.ReadLine();
+            ulong inputMult = Convert.ToUInt64(inputMulti);
+            ulong product = 50 * inputMult;
+            Console.WriteLine("50 * " + inputMult + " = " + product);
+
+            //input a number to add to 25, and print
+            Console.WriteLine("Enter a number to be added to 25:");
+            string inputAddition = Console.ReadLine();
+            ulong inputAdd = Convert.ToUInt64(inputAddition);
+            ulong combined = 25 + inputAdd;
+            Console.WriteLine("25 + " + inputAdd + " = " + combined);
+
+            //input number to be divided by 12.5, and print
+            Console.WriteLine("Enter a number to be devided by 12.5:");
+            string inputDevision = Console.ReadLine();
+            ulong inputDev = Convert.ToUInt64(inputDevision);
+            double quotient = inputDev / 12.5;
+            Console.WriteLine(inputDev + " / 12.5 " + "= " + quotient);
+
+            //input number, check if greater than 50, print boolean value to console
+            Console.WriteLine("Enter a number to determine if it is greater than 50:");
+            string inputComparison = Console.ReadLine();
+            ulong inputComp = Convert.ToUInt64(inputComparison);
+            bool comparison = inputComp > 50;
+            Console.WriteLine(comparison);
+
+            //input a number to be divided by 7, print the remainder (use modulus operator)
+            Console.WriteLine("Enter a number to calculate the remainder after being divided by 7:");
+            string inputModulus = Console.ReadLine();
+            int inputMod = Convert.ToInt16(inputModulus);
+            int modulus = inputMod % 7;
+            Console.WriteLine(modulus);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VariablesExercise.cs/VariablesExercise.cs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VariablesExercise.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" The Tech Academy");
+            Console.WriteLine("Student Daily Report");
+
+            Console.WriteLine("What course are you on?");
+            Console.ReadLine();
+            
+            Console.WriteLine("What page number?");
+            Console.ReadLine();
+
+            Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+            Console.ReadLine();
+
+            Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+            Console.ReadLine();
+
+            Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+            Console.ReadLine();
+
+            Console.WriteLine("How many hours did you study today ? ");
+            Console.ReadLine();
+
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly.Have a great day!");
+
+
+        }
+    }
+}
