04d84edf1f8164ad2c36e0bbe1ef3979fc91fbff
scarb <scarboroughpundor@gmail.com>
2019-12-09 10:21:57 -0600

    program commit

---
+++ Methods_2Nums/Nums.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_2Nums
+{
+    class Nums
+    {
+        public Nums(int num1, int num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+
+        public static void NewMethod(int Int1, int Int2)
+        {
+            Int1++;
+            //Console.WriteLine(Int1);
+            Console.WriteLine(Int2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Methods_2Nums/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_2Nums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate class
+            Nums Nums1 = new Nums(1, 2);
+
+            //call method using 2 numbers
+            Nums.NewMethod(3, 4);
+
+            //call method with parameters by name
+            Nums.NewMethod(Nums1.Num1, Nums1.Num2);
+        }
+    }
+}
