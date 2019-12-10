320ddb7079fc06127cf646ab3da9983d766a4e8f
scarb <scarboroughpundor@gmail.com>
2019-12-10 13:43:13 -0600

    Project commit

---
+++ Methods_OutputParameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_OutputParameters
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            //instantiate ToyParts class
+            ToyPart Robot = new ToyPart("Robot");
+
+            Console.WriteLine("Enter number: ", Robot.Name);
+            Robot.Num = (Convert.ToInt32(Console.ReadLine()));
+            Robot.Beep();
+
+            //overload Boop method (same method name, different arguments)
+            Robot.Boop(out int number1, out int number2);
+            Console.WriteLine("hi",number1, number2);
+            Console.ReadLine();
+
+            Robot.Boop(out string string1, out string string2);
+            Console.WriteLine(string1 + " " + string2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Methods_OutputParameters/ToyPart.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_OutputParameters
+{
+    public class ToyPart
+    {
+        public ToyPart(string name)
+        {
+            Name = name;
+            Num = 1;
+
+        }
+        public string Name { get; set; }
+        public int Num { get; set; }
+
+        public void Beep()//behavior of toypart
+        {
+            Console.WriteLine(Num / 2);
+            Console.ReadLine();
+        }
+
+        //Create method with output parameters
+        public void Boop(out string String1, out string String2)
+        {
+            Console.WriteLine("Please type something: ");
+            String1 = Console.ReadLine();
+
+            Console.WriteLine("Please type something else: ");
+            String2 = Console.ReadLine();
+        }
+
+        public void Boop(out int Num1, out int Num2)
+        {
+            Num1 = Num + 7;
+            Num2 = Num + Num;
+        }
+    }
+}
+
