9b2e2679adbb27dec925086fec2f32c8975844a6
scarb <scarboroughpundor@gmail.com>
2020-01-01 15:41:37 -0600

    commit

---
+++ Generics/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    //1.
+    public class Employee<T> : Person, IQuittable
+    {
+       public int ID { get; set; }
+        //2.
+       public List<T> things { get; set; }//List property with generic data type
+        
+        //overload '==' to compare two objects
+        public static bool operator ==(Employee<T> one, Employee<T> two)
+        {
+            if (one.ID == two.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee<T> one, Employee<T> two)
+        {
+            if (one.ID != two.ID)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        //loop through the list 'things' object property in 'things' with generic data type
+        public void printThings (List<T> things)
+        {
+            foreach (T thing in things)
+            {
+                Console.WriteLine(thing);
+            }
+        }
+
+        //implement the SayName() method with the keyword 'override' to access within the abstract class 'Person'
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("ENTER to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate Employee object
+            Employee<string> name = new Employee<string>();
+            name.firstName = "Sample";
+            name.lastName = "Student";
+            name.ID = 1;
+            name.ID = 2;
+
+            // 3.instantiate 'Employee' object with data type string as it's generic parameter
+            Employee<string> stringThing = new Employee<string>();
+            stringThing.things = new List<string>() { "thing1", "thing2", "thing3", "thing4" };
+            stringThing.printThings(stringThing.things);
+
+            //4. Instantiate an Employee object with type "int" as its generic parameter. 
+            //Assign a list of integers as the property value of things.
+            Employee<int> intThing = new Employee<int>();
+            intThing.things = new List<int>() { 1, 3, 6, 9};
+            intThing.printThings(intThing.things);
+
+            //call SayName() method on 'name' object
+            name.SayName();
+            
+
+            //using polymorphism intantiate object of type 'IQuittable', call QUit() method
+            IQuittable quit = new Employee<string>();
+            quit.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
