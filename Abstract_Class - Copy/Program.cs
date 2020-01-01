fb7ea136f6f6ae73c4f7e51cce8147004b373de8
scarb <scarboroughpundor@gmail.com>
2020-01-01 13:54:50 -0600

    commit

---
+++ Abstract_Class - Copy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate Employee object
+            Employee name = new Employee();
+            name.firstName = "Sample";
+            name.lastName = "Student";
+            name.ID = 1;
+            name.ID = 2;
+
+            //call SayName() method on 'name' object
+            name.SayName();
+            
+
+            //using polymorphism intantiate object of type 'IQuittable', call QUit() method
+            IQuittable quit = new Employee();
+            quit.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OperatorOverloading/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Class
+{
+    public class Employee : Person, IQuittable
+    {
+       public int ID { get; set; }
+        
+        //overload '==' to compare two objects
+        public static bool operator ==(Employee one, Employee two)
+        {
+            if (one.ID == two.ID)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Employee one, Employee two)
+        {
+            if (one.ID != two.ID)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        
+        //implement the SayName() method with the keyword 'override' to access within the abstract class 'Person'
+        public override void SayName()
+        {
+            base.SayName();
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("ENTER to quit...");
+            Console.ReadLine();
+        }
+    }
+}
