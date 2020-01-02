b4bd629db06604c5db0e41db42ce841a29345001
scarb <scarboroughpundor@gmail.com>
2020-01-02 17:39:32 -0600

    Step 137

---
+++ Struct/Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    //create struct called 'Number'
+    public struct Number
+    {
+        //with property data type decimal
+        public decimal Amount { get; set; }
+    }
+}
---
+++ Struct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create object of data type Number & assign an amount to it
+            Number number = new Number();
+            number.Amount = 7.5m;
+
+            //print to console
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
