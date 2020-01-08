28dc505e8ef2691a9add2443e156ec46bf8df2f0
scarb <scarboroughpundor@gmail.com>
2020-01-08 13:40:54 -0600

    TwentyOne

---
+++ TwentyOne/Dealer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TwentyOne
+{
+    public class Dealer
+    {
+        //inheritence is a "is a" relationship not a "has a" - cannot inherit deck because the dealer has a deck, not IS a deck
+        //air on the side of adding a class as a property of another class over inheriting from it
+        //objects mimic real world 
+
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        //takes as an input parameter (hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\scarb\Logs\logs.txt", true))
+            {
+                file.WriteLine(card);
+            }
+            Deck.Cards.RemoveAt(0);
+        }
+    }
+}
---
+++ TwentyOne/IWalkAway.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    interface IWalkAway
+    {
+        TwentyOneDealer Dealer { get; set; }
+
+        //naming convention - start with upper case I 
+        //everything is public in an interface
+        void WalkAway(Player player);
+    }
+}
